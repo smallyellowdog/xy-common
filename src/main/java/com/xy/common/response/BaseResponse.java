8cb0a19ab47d014a9ba13e900e1934b179d190b2
yi.xu <xu3846454@gmail.com>
2019-02-24 14:52:34 +0800

    公共类提交

--- src/main/java/com/xy/common/response/BaseResponse.java
+++ src/main/java/com/xy/common/response/BaseResponse.java
@@ -1,5 +1,6 @@
 package com.xy.common.response;
 
+import com.xy.common.constant.Status;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -7,7 +8,38 @@ import java.util.List;
 @Data
 public class BaseResponse<T> implements Serializable {
     private static final long serialVersionUID = 6281918550231778129L;
-    private int insult;
+    private int code;
     private String msg;
     private List<T> data;
+
+    public static <T>BaseResponse ok(List<T> t){
+        BaseResponse baseResponse = new BaseResponse();
+        baseResponse.setCode(Status.SUCESS.getCode());
+        baseResponse.setMsg(Status.SUCESS.getMsg());
+        baseResponse.setData(t);
+        return baseResponse;
+    }
+
+    public static BaseResponse ok(){
+        BaseResponse baseResponse = new BaseResponse();
+        baseResponse.setCode(Status.SUCESS.getCode());
+        baseResponse.setMsg(Status.SUCESS.getMsg());
+        return baseResponse;
+    }
+
+    public static <T>BaseResponse result(Status status,List<T> t){
+        BaseResponse baseResponse = new BaseResponse();
+        baseResponse.setCode(status.getCode());
+        baseResponse.setMsg(status.getMsg());
+        baseResponse.setData(t);
+        return baseResponse;
+    }
+
+    public static BaseResponse result(Status status){
+        BaseResponse baseResponse = new BaseResponse();
+        baseResponse.setCode(status.getCode());
+        baseResponse.setMsg(status.getMsg());
+        return baseResponse;
+    }
+
 }
